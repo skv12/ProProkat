e834fc8bd32ff89e542195600d08953e25674bfc
Сандал Винокуров <sandal.skv12@gmail.com>
2019-06-03 15:14:08 +0900

    Отчет Pre-Final

--- ProProkat/CreateReportForm.cs
+++ ProProkat/CreateReportForm.cs
@@ -23,48 +23,117 @@ namespace ProProkat
             pp_dbEntities db = new pp_dbEntities();
             var query_closed = db.orders.Where<orders>(f => f.closed_date >= dateTimePickerFrom.Value).Where<orders>(t => t.closed_date <= dateTimePickerTo.Value).Where<orders>(s => s.status == "0");
             var query_active = db.orders.Where<orders>(t => t.date <= dateTimePickerTo.Value).Where<orders>(s => s.status == "1");
+            var query_expired = db.orders.Where<orders>(s => s.status == "3");
             label1.Text = Convert.ToString((dateTimePickerTo.Value - dateTimePickerFrom.Value).TotalHours);
             Excel.Application app = new Excel.Application();
             Excel.Workbook workbook = app.Workbooks.Add(Type.Missing);
             Excel.Worksheet closed_sheet = null;
             app.Visible = true;
+
+            //Лист закрытых заказов
             closed_sheet = workbook.ActiveSheet;
             closed_sheet.Name = "Закрытые заказы";
-            int cell = 3;
-            double cash = 0;
             closed_sheet.Cells[1, 1] = "Выполненные заказы";
-            closed_sheet.Range[closed_sheet.Cells[1, 1], closed_sheet.Cells[1, 2]].Merge();
+            closed_sheet.Range[closed_sheet.Cells[1, 1], closed_sheet.Cells[1, 4]].Merge();
             closed_sheet.Cells[2, 1] = "ID заказа";
             closed_sheet.Cells[2, 2] = "Клиент";
-            
+            closed_sheet.Cells[2, 3] = "Дата открытия";
+            closed_sheet.Cells[2, 4] = "Дата закрытия";
             Excel.Range cellRange = (Excel.Range)closed_sheet.Cells[1, 1];
             Excel.Range rowRange = cellRange.EntireRow;
             Excel.Range colRange = cellRange.EntireColumn;
 
+            int cellRow = 3;
+            double cash = 0;
             foreach (var orders in query_closed)
             {
-                closed_sheet.Cells[cell, 1] = orders.id.ToString();
-                closed_sheet.Cells[cell, 2] = orders.client.ToString();
+                closed_sheet.Cells[cellRow, 1] = orders.id.ToString();
+                closed_sheet.Cells[cellRow, 2] = orders.client.ToString();
+                closed_sheet.Cells[cellRow, 3] = orders.date.ToString();
+                closed_sheet.Cells[cellRow, 4] = orders.closed_date.ToString();
                 DateTime a = orders.closed_date.Value;
                 DateTime b = orders.date.Value;
                 double ddiff = (a-b).TotalHours/24;
                 cash += Convert.ToDouble(Convert.ToDouble(orders.deposit) / 20 * ddiff);
-                cell++;
+                cellRow++;
             }
 
-            closed_sheet.Cells[cell+1, 1] = "Итого выполнено";
-            closed_sheet.Cells[cell+1, 2] = "Заработано";
-            closed_sheet.Cells[cell+2, 1] = cell-3;
-            closed_sheet.Cells[cell+2, 2] = cash;
+            closed_sheet.Cells[cellRow + 1, 1] = "Итого выполнено";
+            closed_sheet.Cells[cellRow + 1, 2] = "Заработано";
+            closed_sheet.Cells[cellRow + 2, 1] = cellRow - 3;
+            closed_sheet.Cells[cellRow + 2, 2] = cash;
             Excel.Range fit_cells = closed_sheet.UsedRange;           
             fit_cells.EntireColumn.AutoFit();
             fit_cells.EntireRow.AutoFit();
             fit_cells.VerticalAlignment = Excel.XlVAlign.xlVAlignCenter;
             fit_cells.HorizontalAlignment = Excel.XlHAlign.xlHAlignCenter;
 
-            Excel.Worksheet active_sheet = null;
+
+            //Лист действующих заказов
+            Excel.Worksheet active_sheet = null; 
+            active_sheet = (Excel.Worksheet)workbook.Worksheets.Add();
             active_sheet = workbook.ActiveSheet;
             active_sheet.Name = "Действующие заказы";
+            active_sheet.Cells[1, 1] = "Действующие заказы";
+            active_sheet.Range[active_sheet.Cells[1, 1], active_sheet.Cells[1, 4]].Merge();
+            active_sheet.Cells[2, 1] = "ID заказа";
+            active_sheet.Cells[2, 2] = "Клиент";
+            active_sheet.Cells[2, 3] = "Дата открытия";
+            active_sheet.Cells[2, 4] = "Cрок заказа";
+            cellRange = (Excel.Range)active_sheet.Cells[1, 1];
+            rowRange = cellRange.EntireRow;
+            colRange = cellRange.EntireColumn;
+
+            cellRow = 3;
+            foreach (var orders in query_active)
+            {
+                active_sheet.Cells[cellRow, 1] = orders.id.ToString();
+                active_sheet.Cells[cellRow, 2] = orders.client.ToString();
+                active_sheet.Cells[cellRow, 3] = orders.date.ToString();
+                active_sheet.Cells[cellRow, 4] = orders.rent.ToString();
+                cellRow++;
+            }
+
+            active_sheet.Cells[cellRow + 1, 1] = "Итого действует";
+            active_sheet.Cells[cellRow + 2, 1] = cellRow - 3;
+            fit_cells = active_sheet.UsedRange;
+            fit_cells.EntireColumn.AutoFit();
+            fit_cells.EntireRow.AutoFit();
+            fit_cells.VerticalAlignment = Excel.XlVAlign.xlVAlignCenter;
+            fit_cells.HorizontalAlignment = Excel.XlHAlign.xlHAlignCenter;
+
+            //Лист просроченных заказов
+            Excel.Worksheet expired_sheet = null;
+            expired_sheet = (Excel.Worksheet)workbook.Worksheets.Add();
+            expired_sheet = workbook.ActiveSheet;
+            expired_sheet.Name = "Просроченные заказы";
+            expired_sheet.Cells[1, 1] = "Просроченные заказы";
+            expired_sheet.Range[expired_sheet.Cells[1, 1], expired_sheet.Cells[1, 4]].Merge();
+            expired_sheet.Cells[2, 1] = "ID заказа";
+            expired_sheet.Cells[2, 2] = "Клиент";
+            expired_sheet.Cells[2, 3] = "Дата открытия";
+            expired_sheet.Cells[2, 4] = "Cрок заказа";
+            cellRange = (Excel.Range)expired_sheet.Cells[1, 1];
+            rowRange = cellRange.EntireRow;
+            colRange = cellRange.EntireColumn;
+
+            cellRow = 3;
+            foreach (var orders in query_expired)
+            {
+                expired_sheet.Cells[cellRow, 1] = orders.id.ToString();
+                expired_sheet.Cells[cellRow, 2] = orders.client.ToString();
+                expired_sheet.Cells[cellRow, 3] = orders.date.ToString();
+                expired_sheet.Cells[cellRow, 4] = orders.rent.ToString();
+                cellRow++;
+            }
+
+            expired_sheet.Cells[cellRow + 1, 1] = "Итого просрочено";
+            expired_sheet.Cells[cellRow + 2, 1] = cellRow - 3;
+            fit_cells = expired_sheet.UsedRange;
+            fit_cells.EntireColumn.AutoFit();
+            fit_cells.EntireRow.AutoFit();
+            fit_cells.VerticalAlignment = Excel.XlVAlign.xlVAlignCenter;
+            fit_cells.HorizontalAlignment = Excel.XlHAlign.xlHAlignCenter;
         }
     }
 }
